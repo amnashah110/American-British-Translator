eec4af894092e227559799a4140601d94efd19a6
Amna Shah <75795497+amnashah110@users.noreply.github.com>
2024-08-01 03:26:14 +0500

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -6,7 +6,7 @@ module.exports = function (app) {
   
   const translator = new Translator();
 
-  app.route('/')
+  app.route('/api/translate')
     .post((req, res) => {
       let translatedText = '';
       const { text, locale } = req.body;
