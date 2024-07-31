7654af79ac01bbf383cc152718c939b8f3cf2ba4
Amna Shah <75795497+amnashah110@users.noreply.github.com>
2024-08-01 03:45:23 +0500

    Create api.js

---
+++ routes/api.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const Translator = require('../components/translator.js');
+
+module.exports = function (app) {
+  
+  const translator = new Translator();
+
+  app.route('/api/translate')
+    .post((req, res) => {
+      let translatedText = '';
+      const { text, locale } = req.body;
+      if(!locale || text === undefined) {
+        return res.json({ error: 'Required field(s) missing' });
+      }
+      if (text === '') {
+        return res.json({ error: 'No text to translate' });
+      }
+      if (locale === 'british-to-american') {
+        translatedText = translator.britishToAmerican(text);
+      } else if (locale === 'american-to-british') {
+        translatedText = translator.americanToBritish(text);
+      } else {
+        return res.json({ error: 'Invalid value for locale field' });
+      }
+      return res.json({text: text, translation: translatedText});
+    });
+};
